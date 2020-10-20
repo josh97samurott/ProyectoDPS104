305e1613ba499539c8a0b3e9c70251147bfbabcc
lefousam-prog <vidarybiper@gmail.com>
2020-10-20 00:35:31 -0600

    chat para usuario con pago

--- cimo/src/app/cimo/chatfrontend/chatfrontend.component.ts
+++ cimo/src/app/cimo/chatfrontend/chatfrontend.component.ts
@@ -19,6 +19,14 @@ export class ChatfrontendComponent implements OnInit {
   commentary:string;
   age:number;
 
+  /*datos para la tabla billing */
+
+  number_card:number;
+  expiration_date_card:string;
+  to_name_card:string;
+  security_code:number;
+  total:number;
+
 
   lista:any=[
     {duracion:"30 minutos", cantidad:30},
@@ -37,9 +45,25 @@ export class ChatfrontendComponent implements OnInit {
   constructor(private service:ServiceService, private router:Router) { }
 
   ngOnInit(): void {
+    this.total=0;
   }
 
   registerChat(registervideoForm: NgForm){
+
+       //pago por hora de consulta el pago base es 8$
+    /* 30 minutos 8$
+       1 hora 16$
+       2 horas 24$ */
+
+       if(this.duration==30){
+        this.total=8;
+      }
+      else if(this.duration==60){
+        this.total=16;
+      }
+      else if(this.duration==120){
+        this.total=24;
+      }
     
     var data={
       id_pacient:2,
@@ -49,7 +73,15 @@ export class ChatfrontendComponent implements OnInit {
       start_session:this.start_session,
       duration:this.duration,
       type_date:"chat en linea",
-      commentary:this.commentary
+      commentary:this.commentary,
+      //aqui comienzan los datos para billing de pago y se les adjuntara date
+      description:"Compra de sesión de chat en linea",
+      number_card:this.number_card,
+      expiration_date_card:this.expiration_date_card,
+      to_name_card:this.to_name_card,
+      security_code:this.security_code,
+      total:this.total,
+      state:"espera"
       
     }
 
@@ -74,6 +106,12 @@ export class ChatfrontendComponent implements OnInit {
     this.age=null;
     this.start_session= null;
     this.commentary=null;
+
+    this.number_card=null;
+    this.expiration_date_card=null;
+    this.to_name_card=null;
+    this.security_code=null;
+    this.total=0;
 
   }
 
